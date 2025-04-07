20aa0d5c5e32c8befebba2e95dcf4c770180c2ba
carter-oleary-mssa <carteroleary.mssa@gmail.com>
2025-04-07 17:15:42 -0400

    Assignment 1.1.3

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter two numbers to be divided: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{a} / {b} = {(a/b)}r{a%b}");
