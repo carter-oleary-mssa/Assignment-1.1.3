b7f463e44072268eb2c5601aca43b8fbd6e35e9e
carter-oleary-mssa <carteroleary.mssa@gmail.com>
2025-04-08 11:09:14 -0400

    changed var names

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Enter two numbers to be divided: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"{a} / {b} = {(a/b)}r{a%b}");
